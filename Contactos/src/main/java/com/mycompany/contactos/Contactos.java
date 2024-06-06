fdc942e819e127e2d96ef972575b2b66c838f651
Jlplozada <jose_lpaezL@soy.sena.edu.co>
2024-06-05 20:50:36 -0500

    visual con bean

---
+++ Contactos/src/main/java/com/mycompany/contactos/Contactos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contactos;
+
+/**
+ *
+ * @author jlloz
+ */
+public class Contactos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
